98302dd0b0a2227ac7425d7c9b50cb4459261ac1
Frank Licona <f.a.licona@hotmail.com>
2019-03-28 06:29:13 -0500

    agregando calendario

--- view/footer.php
+++ view/footer.php
@@ -29,20 +29,24 @@
         </div>
     </div>
 </footer>
+
 <script src="assets/js/core/jquery.min.js"></script>
 <script src="assets/js/core/popper.min.js"></script>
 <script src="assets/js/core/bootstrap.min.js"></script>
 <script src="assets/js/plugins/perfect-scrollbar.jquery.min.js"></script>
 <script src="assets/js/loader.js"></script>
+<script src="node_modules\fullcalendar\dist\locale\es.js"></script>
+<script src="node_modules\fullcalendar\dist\fullcalendar.min.js"></script>
+<script src="node_modules\moment\src\moment.js"></script>
+<script>
+    $(document).ready(function(){
+        $('#Calendar').fullCalendar();
+    });
+</script>
 
-<!-- Chart JS -->
-<script src="assets/js/plugins/chartjs.min.js"></script>
-<!--  Notifications Plugin    -->
 <script src="assets/js/plugins/bootstrap-notify.js"></script>
 <!-- Control Center for Now Ui Dashboard: parallax effects, scripts for the example pages etc -->
 <script src="assets/js/now-ui-dashboard.min.js?v=1.3.0" type="text/javascript"></script>
-<!-- Now Ui Dashboard DEMO methods, don't include it in your project! -->
-<script src="assets/demo/demo.js"></script>
 <!--  Notifications Plugin    -->
 <script src="assets/js/plugins/bootstrap-notify.js"></script>
 <!-- Control Center for Now Ui Dashboard: parallax effects, scripts for the example pages etc -->
@@ -77,7 +81,7 @@ if (isset($_SESSION['auth'])) {
         type: 'success',
         layout: 'bottomRight',
         theme: 'metroui',
-        text: '".$_SESSION['name']."Ha iniciado sesion satisfactoriamente',
+        text: '" . $_SESSION['name'] . "Ha iniciado sesion satisfactoriamente',
         timeout: '4000',
         progressBar: true,
         closeWith: ['click'],
